bcaf33971554a4dee944faac6612e7d706c2353b
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2023-09-23 01:24:02 -0500

    Mejorando detalles

--- Clock_center01/script.js
+++ Clock_center01/script.js
@@ -3,14 +3,13 @@ function startTime() {
   var hr = today.getHours();
   var min = today.getMinutes();
   var sec = today.getSeconds();
-  ap = hr < 12 ? "<span>AM</span>" : "<span>PM</span>";
   hr = hr == 0 ? 12 : hr;
   hr = hr > 12 ? hr - 12 : hr;
 
   hr = checkTime(hr);
   min = checkTime(min);
   sec = checkTime(sec);
-  document.getElementById("clock").innerHTML = hr + ":" + min + " " + ap;
+  document.getElementById("clock").innerHTML = hr + ":" + min;
 
   var months = [
     "Enero",
