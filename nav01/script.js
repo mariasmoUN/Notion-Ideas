ec3ec358983cffd711533cab8699a1a44d0d80e1
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2023-09-22 19:52:52 -0500

    Create home link

---
+++ nav01/script.js
@@ -0,0 +1,14 @@
+let direction = "vertical";
+if (direction === "vertical") {
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    `<style>.navMenu a {display: block; text-align:left; margin:7px} i{width:15px; margin-right:5px} .navMenu{margin:3px}</style>`
+  );
+}
+
+function navigate(id) {
+  if (id == "home")
+    window
+      .open("https://www.notion.so/7174b3a65d064b44a234680dd749b564", "_blank")
+      .focus();
+}
